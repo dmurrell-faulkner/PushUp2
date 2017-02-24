aeb39e820c8d069e6067b52a908e8d7139c6b063
David Murrell <dmurrell@faulkner.edu>
2017-02-24 13:36:08 -0600

    Initial commit

---
+++ app/src/main/java/com/example/murrell/pushup2/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.murrell.pushup2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    private void testup(){
+        int i = 1 + 1;
+    }
+}
